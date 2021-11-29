aaae6d06366f9e8b142f66de7bdffd7b8406b112
MarkWightman02 <Markwightman02@gmail.com>
2021-11-29 13:46:07 -0500

    Updated .h file

--- b-tree.h
+++ b-tree.h
@@ -1,35 +1,57 @@
 #include <iostream>
+#include <vector>
 
 class BTree{
 
+//Private class members of the tree include
+//a pointer to the root node and the # of keys in a node
 private:
-
-    int t;
+    Node *root;
+    unsigned int degree;
 
 public:
 
-    BTree():
-    ~BTree();
-    BTree(int t);
+    //Create an empty B Tree
+    BTree(int degree);
+    //Insert data into the Tree
     void insert(int data);
-    void search(int data);
+    //Search for a key within the tree and return the node it's at
+    Node* search(int data);
+    //Traverse the tree
+    void traverse();
 
 
-friend class Node;
 };
 
 
 class Node{
 
 private:
+    //Current # of keys in node
+    int current;
+    //True if node is a leaf
+    bool leaf;
+    //Vector of keys in the node
+    std::vector<int> keys;
+    //Vector of the node's children
+    std::vector<*Node> children;
+    //max degree of node
+    unsigned int degree;
 
-   bool leaf;
 
 public:
-
-    Node();
-    Node(bool leaf);
+    //Constructor
+    Node(int data, bool leaf);
+    //Insert when not full
+    void insertNotFull(int data);
+    //Traverse tree with this node as root
     void traverse();
-    void splitChild();
+    //When the child is full, this function will split it, index is the index of the child array
+    void splitChild(int index, Node *child);
+    //Find a key in a tree with this node as root
+    Node* search(int data);
+
+    //Make BTree a friend
+    friend class BTree;
 
 };
