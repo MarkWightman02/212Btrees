f115fada13a8aa170bcd90894b18c290d00f80f0
jasonccarroll <54959897+jasonccarroll@users.noreply.github.com>
2021-12-06 10:21:19 -0500

    Add files via upload

--- b-tree.cpp
+++ b-tree.cpp
@@ -17,7 +17,18 @@ Node* BTree::search(int data){
 }
 
 void traverse(){
-  
+    //for loop runs through 'current' children
+    for (int i = 0; i < current; i++) {
+        // If the current node is not a leaf, traverse the subtree rooted with children[i] then print key[i],
+        if (leaf == false) {
+            chidren[i]->traverse();
+        }
+        cout << " " << keys[i];
+    }
+    // Print the subtree rooted with last child
+    if (leaf == false) {
+        children[i]->traverse();
+    }
 }
 
 //Node Class
