f3d2cd378d12e7abe220bda2e9d8fcc2babfd893
jasonccarroll <54959897+jasonccarroll@users.noreply.github.com>
2021-12-07 12:38:12 -0500

    Update traverse.cpp

--- traverse.cpp
+++ traverse.cpp
@@ -1,13 +1,15 @@
-void BTreeNode::traverse()
+void Node::traverse()
 {
     //for loop runs through 'current' children
     for (int i = 0; i < current; i++) {
         // If the current node is not a leaf, traverse the subtree rooted with children[i] then print key[i],
-        if (leaf == false)
+        if (leaf == false) {
             chidren[i]->traverse();
+        }
         cout << " " << keys[i];
     }
     // Print the subtree rooted with last child
-    if (leaf == false)
+    if (leaf == false) {
         children[i]->traverse();
+    }
 }
