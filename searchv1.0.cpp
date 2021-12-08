942299402f6de33f30037cb61be2419743e4ae32
jasonccarroll <54959897+jasonccarroll@users.noreply.github.com>
2021-12-08 18:35:31 -0500

    Update searchv1.0.cpp

--- searchv1.0.cpp
+++ searchv1.0.cpp
@@ -5,11 +5,13 @@ Node *Node::search(int data)
     while ((i < current) && (data > keys[i])){
         i++;
         // If the current key = input, return this node
-        if (keys[i] == data)
+        if (keys[i] == data) {
             return this;
+        }
         // if this is a leaf node and the key is not here
-        if (leaf == true)
+        if (leaf == true) {
             return NULL;
+        }
         // recursive call using children of i as the data parameter
         return children[i]->search(data);
     }
